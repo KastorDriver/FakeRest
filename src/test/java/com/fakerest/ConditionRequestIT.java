54c19615d3fbd3bb1b4fe61764f29cdf6eff498c
Kastor <KastorDriver@gmail.com>
2017-02-06 23:28:00 +0200

    post contentLength test

--- src/test/java/com/fakerest/ConditionRequestIT.java
+++ src/test/java/com/fakerest/ConditionRequestIT.java
@@ -78,23 +78,24 @@ public class ConditionRequestIT {
     public void correctAnswerFromContentLengthCondition() throws Exception {
         final String CONDITION_RESPONSE_TEXT = "condition response text";
         final int CONDITION_STATUS_CODE = 201;
+        final String REQUEST_BODY = "requestBody";
 
         String route = "--- !com.fakerest.bean.Route\n" +
-                "method: get\n" +
+                "method: post\n" +
                 "url: " + PATH + "\n" +
                 "defaultAnswer: !com.fakerest.bean.Answer\n" +
                 "  status: 200\n" +
                 "  body: response text\n" +
                 "conditions:\n" +
                 "  - !com.fakerest.bean.Condition\n" +
-                "    condition: @contentLength >= 1024\n" +
+                "    condition: @contentLength == " + REQUEST_BODY.length() + "\n" +
                 "    answer:\n" +
                 "      status: " + CONDITION_STATUS_CODE + "\n" +
                 "      body: " + CONDITION_RESPONSE_TEXT + "\n";
 
         PowerMockito.stub(PowerMockito.method(Server.class, LOAD_ROUTES_FUNC_NAME)).toReturn(route);
         Server.main(null);
-        ResponseEntity<String> response = new RestTemplate().getForEntity(URL + PATH, String.class);
+        ResponseEntity<String> response = new RestTemplate().postForEntity(URL + PATH, REQUEST_BODY, String.class);
         assertEquals(CONDITION_STATUS_CODE, response.getStatusCodeValue());
         assertEquals(CONDITION_RESPONSE_TEXT, response.getBody());
     }
