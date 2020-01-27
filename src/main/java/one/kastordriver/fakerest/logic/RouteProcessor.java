eed9ca4aa9e16fa5ad114ca0e7d6581dc6fb94e6
KastorDriver <KastorDriver@gmail.com>
2020-01-27 19:39:55 +0200

    refactoring

--- src/main/java/one/kastordriver/fakerest/logic/RouteProcessor.java
+++ src/main/java/one/kastordriver/fakerest/logic/RouteProcessor.java
@@ -39,7 +39,7 @@ public class RouteProcessor {
     }
 
     private Answer fetchAnswer(Route route, Optional<Condition> suitableCondition) {
-        return suitableCondition.isPresent() ? suitableCondition.get().getAnswer() : route.getAnswer();
+        return suitableCondition.map(Condition::getAnswer).orElse(route.getAnswer());
     }
 
     private Object processAnswer(Answer answer, Response response) {
