941bff7bdfa4ca2eedf2a288112013963d4d296e
KastorDriver <KastorDriver@gmail.com>
2021-09-21 13:12:08 +0300

    excess @Tolerate annotation

--- src/main/java/one/kastordriver/fakerest/model/Cookie.java
+++ src/main/java/one/kastordriver/fakerest/model/Cookie.java
@@ -7,13 +7,10 @@ import lombok.experimental.Tolerate;
 @Builder
 @Data
 public class Cookie {
+
     private String path;
     private String name;
     private String value;
     private int maxAge = -1;
     private boolean secure;
-
-    //TODO do we need this?
-    @Tolerate
-    public Cookie() {}
 }
