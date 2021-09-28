88ece5eebdfec3717e0fceedf8569dc68101b2bd
KastorDriver <KastorDriver@gmail.com>
2021-09-28 19:25:16 +0300

    default response code

--- src/main/java/one/kastordriver/fakerest/model/RouteResponse.java
+++ src/main/java/one/kastordriver/fakerest/model/RouteResponse.java
@@ -17,9 +17,10 @@ import java.util.Map;
 public class RouteResponse {
 
     private static final String EMPTY_RESPONSE_BODY = "";
+    private static final int DEFAULT_RESPONSE_CODE = 200;
 
     private String body = EMPTY_RESPONSE_BODY;
-    private Integer status = 200;
+    private Integer status = DEFAULT_RESPONSE_CODE;
     private Map<String, String> headers = new HashMap<>();
     private List<Cookie> cookies = new ArrayList<>();
     private List<String> removeCookies = new ArrayList<>();
